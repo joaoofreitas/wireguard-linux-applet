7af6a45a1f21f209610b7e6c65d56760f8fb68ab
joaoofreitas <joaoofreitas00@gmail.com>
2020-10-19 23:53:43 +0100

    Functions Ready

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+extern crate systray;
+extern crate gtk;
+
+#[cfg(target_os = "linux")]
+fn main() -> Result<(), systray::Error> {
+    let mut app;
+    match systray::Application::new() {
+        Ok(w) => app = w,
+        Err(_) => panic!("Can't create window!"),
+    }
+   
+   //need to check this. 
+    app.set_icon_from_file("/home/johnny/Documents/Projects/wireguard-linux-applet/assets/wg.svg")?;
+
+    
+    app.add_menu_item("Turn On.", |_| {
+        println!("Turning it on.");
+        Ok::<_, systray::Error>(())
+    })?;
+
+    app.add_menu_item("Import Config File", |_| {
+        println!("Importing Config.");
+        Ok::<_, systray::Error>(())
+    })?;
+
+    app.add_menu_separator()?;
+
+    app.add_menu_item("Quit", |window| {
+        window.quit();
+        Ok::<_, systray::Error>(())
+    })?;
+
+    println!("Waiting on message!");
+    app.wait_for_message()?;
+    Ok(())
 }
