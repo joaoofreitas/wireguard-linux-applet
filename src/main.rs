f38696e6930d2a880d25c6e352dd807b50b6b688
joaoofreitas <joaoofreitas00@gmail.com>
2020-10-20 01:05:13 +0100

    Missing std and shell commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 extern crate systray;
-extern crate gtk;
 
 #[cfg(target_os = "linux")]
 fn main() -> Result<(), systray::Error> {
@@ -9,16 +8,17 @@ fn main() -> Result<(), systray::Error> {
         Err(_) => panic!("Can't create window!"),
     }
    
-   //need to check this. 
-    app.set_icon_from_file("/home/johnny/Documents/Projects/wireguard-linux-applet/assets/wg.svg")?;
+   //Get the stdout of echo $PWD and add /assets/wg.svg 
+    app.set_icon_from_file("$PWD/assets/wg.svg")?;
 
-    
-    app.add_menu_item("Turn On.", |_| {
+   //Run: tunsafe wg.conf & 
+    app.add_menu_item("Turn On", |_| {
         println!("Turning it on.");
         Ok::<_, systray::Error>(())
     })?;
-
-    app.add_menu_item("Import Config File", |_| {
+    
+    //Run: xdg-open $PWD
+    app.add_menu_item("Place confiiguration file", |_| {
         println!("Importing Config.");
         Ok::<_, systray::Error>(())
     })?;
@@ -34,3 +34,18 @@ fn main() -> Result<(), systray::Error> {
     app.wait_for_message()?;
     Ok(())
 }
+
+/*
+ * To do:
+ *      - Solve Comments
+ *      - Add in App About
+ *      - Write and installation script for building the binaries
+ *      - Write Docs
+ *
+ *
+ *
+ *
+ *
+ */
+
+
